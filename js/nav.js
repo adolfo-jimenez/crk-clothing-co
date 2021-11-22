4dc7b94a2209187c9b1754046c165f3e66d6563a
CrazyAl <dlfbonds@gmail.com>
2021-11-21 20:56:44 -0800

    Index page upload.

---
+++ js/nav.js
@@ -0,0 +1,13 @@
+function toggleMenu() {
+    const menuToggle = document.querySelector('.toggle')
+    const navigation = document.querySelector('.nav')
+    menuToggle.classList.toggle('active')
+    navigation.classList.toggle('active')
+}
+
+// let toggleNavState = false;
+
+// let toggleNav = function () {
+// const navigation = document.querySelector('.nav')
+    
+// }
