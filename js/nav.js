049befb8aa231c266b4cac3088ceadaa3dd1d979
CrazyAl <dlfbonds@gmail.com>
2021-12-12 14:09:35 -0800

    temp nav bar for mobile.

--- js/nav.js
+++ js/nav.js
@@ -1,13 +1,21 @@
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+});
 function toggleMenu() {
     const menuToggle = document.querySelector('.toggle')
     const navigation = document.querySelector('.nav')
+    const logoFont = document.querySelector('.logo')
+    const shopCart = document.querySelector('.cart')
     menuToggle.classList.toggle('active')
     navigation.classList.toggle('active')
+    logoFont.classList.toggle('active')
+    shopCart.classList.toggle('active')
 }
-
-// let toggleNavState = false;
-
-// let toggleNav = function () {
-// const navigation = document.querySelector('.nav')
-    
-// }
